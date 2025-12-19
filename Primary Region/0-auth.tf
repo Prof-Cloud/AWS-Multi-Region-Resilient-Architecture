8c21d780c0f0a33f99562883dfd7147325d5bbe4
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-18 20:57:56 -0500

    Add files via upload

--- Primary Region/0-auth.tf
+++ Primary Region/0-auth.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+
+  default_tags {
+    tags = {
+      TeamMember  = "Prof Cloud"
+      ManagedBy   = "Terraform"
+      Environment = "Dev"
+      Location    = "North Virginia"
+    }
+  }
+}
 
