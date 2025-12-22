5f1618127e71d474f85d9dea8c8ff1f64c676725
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-22 18:23:39 -0500

    Add files via upload

---
+++ 32.vpcflowlog(2nd).tf
@@ -0,0 +1,16 @@
+#Create VPC Flow Logs
+#Secondary Region
+resource "aws_flow_log" "vpc_flow_2nd" {
+  provider                 = aws.London
+  log_destination          = aws_s3_bucket.log_bucket.arn
+  log_destination_type     = "s3"
+  traffic_type             = "ALL"
+  vpc_id                   = aws_vpc.secondary_vpc.id
+  max_aggregation_interval = 60
+
+  tags = {
+    Name = "Secondary VPC Flow Logs"
+  }
+}
+
+
