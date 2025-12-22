f040182808403c6ce3e83fddc3d1b6566ce19085
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-22 18:23:00 -0500

    Add files via upload

---
+++ 26.asg(2nd).tf
@@ -0,0 +1,62 @@
+#Creates Auto Scaling 
+#Secondary Region
+resource "aws_autoscaling_group" "app_asg_2nd" {
+  provider            = aws.London
+  name_prefix         = "app_asg_2nd"
+  min_size            = 1
+  max_size            = 3 #Kept smaller for standby region
+  desired_capacity    = 2
+  vpc_zone_identifier = aws_subnet.private_subnet_2nd[*].id
+
+  #Health check
+  #Secondary Region
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+  force_delete              = true
+  target_group_arns         = [aws_lb_target_group.app_tg_2nd.arn]
+
+  launch_template {
+    id      = aws_launch_template.app_template_2nd.id
+    version = "$Latest"
+  }
+
+  enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+
+  # Instance protection for launching
+  #Secondary Region
+  initial_lifecycle_hook {
+    name                  = "instance-protection-launch"
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    notification_metadata = "{\"key\":\"value\"}"
+  }
+
+  # Instance protection for terminating
+  #Secondary Region
+  initial_lifecycle_hook {
+    name                 = "scale-in-protection"
+    lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+    default_result       = "CONTINUE"
+    heartbeat_timeout    = 300
+  }
+
+}
+
+# Auto Scaling Policy
+#Secondary Region
+resource "aws_autoscaling_policy" "asg_policy_2nd" {
+  provider               = aws.London
+  name                   = "app1-cpu-target"
+  autoscaling_group_name = aws_autoscaling_group.app_asg_2nd.name
+
+  policy_type               = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 75.0
+  }
+}
---
+++ 27.cloudwatch(2nd).tf
@@ -0,0 +1,51 @@
+#Log group 
+#Secondary Region
+resource "aws_cloudwatch_log_group" "app_logs_2nd" {
+  provider          = aws.London
+  name              = "/aws/ec2/application"
+  retention_in_days = 7 # Automatically deletes logs older than 7 days to save costs
+}
+
+# CPU Utilization Alarm
+#Secondary Region
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu_2nd" {
+  provider            = aws.London
+  alarm_name          = "EC2_CPU_Utilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "Alarm if CPU > 80% for 10 minutes"
+  treat_missing_data  = "notBreaching"
+
+  #Monitor the Whole ASG 
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg_2nd.name
+  }
+
+  alarm_actions = [aws_sns_topic.alerts_2nd.arn]
+}
+
+# Network In Alarm
+#Secondary Region
+resource "aws_cloudwatch_metric_alarm" "ec2_network_in_2nd" {
+  provider            = aws.London
+  alarm_name          = "EC2_Network_In"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "NetworkIn"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 10000000 # adjust as needed
+  alarm_description   = "Alarm if NetworkIn exceeds threshold"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg_2nd.name
+  }
+  alarm_actions = [aws_sns_topic.alerts_2nd.arn]
+}
