4d17c8f1f171b1fcc585184a136ceb7d7e269887
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-28 20:55:27 -0500

    Add files via upload

---
+++ 35.SSL certificate.tf
@@ -0,0 +1,45 @@
+#Requesting SSL certificate
+#cert on available in us-east-01
+resource "aws_acm_certificate" "cert" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
+
+
+  tags = {
+    Name = "SSL Cert"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Validate cert before attaching to HTTPS listener
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
+
+  # Ensure CNAMEs exist first
+  depends_on = [aws_route53_record.cert_validation]
+
+  
+} 
+
+#ACM certificate for London 
+resource "aws_acm_certificate" "cert_2nd" {
+  provider          = aws.London
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+}
+
+
+#Validate cert before attaching to HTTPS listener
+resource "aws_acm_certificate_validation" "cert_validation_2nd" {
+  provider                = aws.London
+  certificate_arn         = aws_acm_certificate.cert_2nd.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_2nd : record.fqdn]
+
+  # Ensure CNAMEs exist first
+  depends_on = [aws_route53_record.cert_validation]
+}
