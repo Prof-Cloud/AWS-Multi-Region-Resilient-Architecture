3793b7c0a3bc3e0edb3fcb5612539379366c78f9
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-31 20:14:09 -0500

    Add files via upload

--- 23.launchtemplate(2nd).tf
+++ 23.launchtemplate(2nd).tf
@@ -11,28 +11,27 @@ resource "aws_launch_template" "app_template_2nd" {
 
   vpc_security_group_ids = [aws_security_group.Linux_Server_2nd.id]
 
-  user_data = base64encode(templatefile("userdata.sh", {
 
-  
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile_2nd.name
+  }
+
+
+  user_data = base64encode(templatefile("userdata_london.sh", {
+
     db_endpoint = aws_rds_cluster.secondary_cluster.endpoint
     db_name     = aws_rds_global_cluster.global_db.database_name
     db_user     = aws_rds_cluster.primary_cluster.master_username
     db_password = aws_secretsmanager_secret_version.db_password_val.secret_string
   }))
 
-
-
   lifecycle {
     create_before_destroy = true
   }
 
-  #Attach Instance Profile
-  #Secondary Region
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ec2_profile.name
-  }
-
   tags = {
-    Name = "EC2 Launch Template - Secondary Region"
+    Name = "EC2 Launch Template London"
   }
 }
+
+
