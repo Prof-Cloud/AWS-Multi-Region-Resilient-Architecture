748e66c857e282a4447f9df97699be80d5d6d392
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-03 13:57:48 -0500

    Add files via upload

--- 11.cloudwatch.tf
+++ 11.cloudwatch.tf
@@ -1,36 +1,50 @@
-# CloudWatch alarm for ALB Errors
-resource "aws_cloudwatch_metric_alarm" "primary_health_check" {
-  alarm_name          = "primary-health-check"
+#Cloudwatch - 0 EC2 alarm
+#Watches the Primary Target Group and sounds the alarm if EC2s are deleted.
+resource "aws_cloudwatch_metric_alarm" "no_healthy_hosts" {
+  alarm_name          = "primary-tg-no-hosts"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "HealthCheckStatus"
-  namespace           = "AWS/Route53"
+  evaluation_periods  = "1"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
   period              = "60"
   statistic           = "Minimum"
-  threshold           = "0" #Any 500 error triggers it
-  alarm_description   = "This metric monitors the health check status"
-  treat_missing_data  = "breaching"
+  threshold           = "1" ## If it drops below 1, it's a fail
 
   dimensions = {
-    HealthCheckId = aws_route53_health_check.primary.id
+    # This points specifically to your Primary ALB and Target Group
+    TargetGroup  = aws_lb_target_group.app_tg.arn_suffix
+    LoadBalancer = aws_lb.primary_alb.arn_suffix
   }
 
-  # This triggers the email AND the Lambda failover script
-  alarm_actions = [aws_sns_topic.route53_failover_alerts.arn]
+  alarm_description = "Fails when there are no EC2s in the primary target group"
 
+  # If the metric disappears (like when you delete the TG), treat it as a failure
+  treat_missing_data = "breaching"
+}
 
-  tags = {
-    Name = "CloudWatch Primary Health Check"
+# Cloudwatch - High Latency Alarm
+# This catches the 2xx warnings, site is up but responding too slowly
+resource "aws_cloudwatch_metric_alarm" "high_latency" {
+  alarm_name          = "primary-high-latency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "TargetResponseTime"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "2.0" # If it takes more than 2 seconds, it's a warning/fail
+
+  dimensions = {
+    LoadBalancer = aws_lb.primary_alb.arn_suffix
   }
+
+  alarm_description  = "Fails if the app is crawling (slow 2xx responses)"
+  treat_missing_data = "notBreaching"
 }
 
-#Log group 
-resource "aws_cloudwatch_log_group" "app_logs" {
-  name              = "/aws/ec2/application"
-  retention_in_days = 7 # Automatically deletes logs older than 7 days to save costs
-}
 
-# CPU Utilization Alarm
+# CLoudWatch - CPU Utilization Alarm
+#Montior CPU for ALB
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   alarm_name          = "EC2_CPU_Utilization"
   comparison_operator = "GreaterThanThreshold"
@@ -69,3 +83,10 @@ resource "aws_cloudwatch_metric_alarm" "ec2_network_in" {
   }
   alarm_actions = [aws_sns_topic.alerts.arn]
 }
+
+#Log group 
+resource "aws_cloudwatch_log_group" "app_logs" {
+  name              = "/aws/ec2/application"
+  retention_in_days = 7 # Automatically deletes logs older than 7 days to save costs
+}
+
