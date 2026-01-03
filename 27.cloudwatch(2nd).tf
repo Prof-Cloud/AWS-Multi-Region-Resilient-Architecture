9f541edf6639d0a0195717e49a748679b4ced9a4
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-03 13:58:22 -0500

    Add files via upload

--- 27.cloudwatch(2nd).tf
+++ 27.cloudwatch(2nd).tf
@@ -1,9 +1,47 @@
-#Log group 
+
+#Cloudwatch - 0 EC2 alarm
 #Secondary Region
-resource "aws_cloudwatch_log_group" "app_logs_2nd" {
-  provider          = aws.London
-  name              = "/aws/ec2/application"
-  retention_in_days = 7 # Automatically deletes logs older than 7 days to save costs
+#Watches the Primary Target Group and sounds the alarm if EC2s are deleted.
+resource "aws_cloudwatch_metric_alarm" "no_healthy_hosts_2nd" {
+  provider            = aws.London
+  alarm_name          = "secondary-tg-no-hosts"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "1"
+
+  dimensions = {
+    TargetGroup  = aws_lb_target_group.alb-tg_2nd.arn_suffix
+    LoadBalancer = aws_lb.secondary_alb_2nd.arn_suffix
+  }
+
+  alarm_description  = "Fails if London target group is empty"
+  treat_missing_data = "breaching"
+}
+
+# Cloudwatch - High Latency Alarm
+#Secondary Region
+# This catches the 2xx warnings, site is up but responding too slowly
+resource "aws_cloudwatch_metric_alarm" "high_latency_2nd" {
+  provider            = aws.London
+  alarm_name          = "secondary-high-latency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "TargetResponseTime"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "2.0"
+
+  dimensions = {
+    LoadBalancer = aws_lb.secondary_alb_2nd.arn_suffix
+  }
+
+  alarm_description  = "Fails if London app is crawling/slow"
+  treat_missing_data = "notBreaching"
 }
 
 # CPU Utilization Alarm
@@ -13,7 +51,7 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_2nd" {
   alarm_name          = "ASG_CPU_Utilization_London"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
-  metric_name         = "GroupAverageCPUUtilization"
+  metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = 300
   statistic           = "Average"
@@ -48,4 +86,13 @@ resource "aws_cloudwatch_metric_alarm" "ec2_network_in_2nd" {
     AutoScalingGroupName = aws_autoscaling_group.app_asg_2nd.name
   }
   alarm_actions = [aws_sns_topic.alerts_2nd.arn]
+}
+
+
+#Log group 
+#Secondary Region
+resource "aws_cloudwatch_log_group" "app_logs_2nd" {
+  provider          = aws.London
+  name              = "/aws/ec2/application"
+  retention_in_days = 7 # Automatically deletes logs older than 7 days to save costs
 }
