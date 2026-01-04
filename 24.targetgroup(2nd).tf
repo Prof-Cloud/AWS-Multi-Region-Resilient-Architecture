4a4be4b029073fdef8fab8fccb13ab12d7513a7e
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-04 13:06:10 -0500

    Add files via upload

--- 24.targetgroup(2nd).tf
+++ 24.targetgroup(2nd).tf
@@ -1,6 +1,6 @@
 # Create a target group for the ALB
 #Secondary Region
-resource "aws_lb_target_group" "tg_london" {
+resource "aws_lb_target_group" "alb_tg_2nd" {
   provider    = aws.London
   name        = "tg-london"
   port        = 80
@@ -14,8 +14,8 @@ resource "aws_lb_target_group" "tg_london" {
     path                = "/health"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 30
-    timeout             = 5
+    interval            = 15
+    timeout             = 10
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
