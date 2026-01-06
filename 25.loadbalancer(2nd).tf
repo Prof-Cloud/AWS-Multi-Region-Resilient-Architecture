96b41e748dfd0a708617e25440af9f6904ab2343
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-06 17:35:10 -0500

    Add files via upload

--- 25.loadbalancer(2nd).tf
+++ 25.loadbalancer(2nd).tf
@@ -22,6 +22,21 @@ resource "aws_lb" "secondary_alb_2nd" {
   }
 }
 
+#Redirect HTTP to HTTPS
+#Ensure all users are redirected to secure HTTPS URL
+resource "aws_lb_listener" "http_2nd" {
+  provider          = aws.London
+  load_balancer_arn = aws_lb.secondary_alb_2nd.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # Direct forward for testing; no redirect to 443 yet
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg_2nd.arn
+  }
+}
+
 #Create HTTPS Listerner for ALB 
 #This Listens traffic on Port 443 and forwards it to the secondary target group
 resource "aws_lb_listener" "https_2nd" {
@@ -31,35 +46,11 @@ resource "aws_lb_listener" "https_2nd" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate.cert_2nd.arn
+  depends_on        = [aws_acm_certificate_validation.cert_validation_2nd]
 
-  # Wait for validation
-  depends_on = [aws_acm_certificate_validation.cert_validation_2nd]
-
-  #The default is to forward traffic to the secondary EC2 instsnces
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg_2nd.arn
-  }
-}
-
-#Redirect HTTP to HTTPS
-#Ensure all users are redirected to secure HTTPS URL
-resource "aws_lb_listener" "http_redirect_2nd" {
-  provider          = aws.London
-  load_balancer_arn = aws_lb.secondary_alb_2nd.arn
-  port              = 80
-  protocol          = "HTTP"
-
-
-  #The default is to forward traffic to HTTPS
-  default_action {
-    type = "redirect"
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
-
   }
 }
 
