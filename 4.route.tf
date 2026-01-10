ad4d567f3c993fb9314c5dcca01803bd4dd7d387
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-10 18:51:59 -0500

    Add files via upload

--- 4.route.tf
+++ 4.route.tf
@@ -22,6 +22,9 @@ resource "aws_route" "private_nat" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
+
+  # Ensure the NAT Gateway is fully functional before creating the route
+  depends_on = [aws_nat_gateway.nat]
 }
 
 # Route table associations for public subnets
