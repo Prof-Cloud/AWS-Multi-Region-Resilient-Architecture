49e3ef83976c2d8d80f790d71728d0a474a7d1a7
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-11 17:08:16 -0500

    Add files via upload

--- 9.asg.tf
+++ 9.asg.tf
@@ -7,10 +7,10 @@ resource "aws_autoscaling_group" "app_asg" {
   vpc_zone_identifier = aws_subnet.private_subnet[*].id
 
   #Set a specific name that matches the Launch Template variable
-  name                = "vanish-app-asg-primary"
+  name = "vanish-app-asg-primary"
 
   #Health check
-  health_check_type         = "ELB"
+  health_check_type = "ELB"
 
   #To get Lodon region live faster, reduce the grace period
   health_check_grace_period = 40
@@ -25,13 +25,31 @@ resource "aws_autoscaling_group" "app_asg" {
   enabled_metrics = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
 
   # Instance protection for launching
+  #This keeps the instance in 'Pending:Wait' until UserData signals
   initial_lifecycle_hook {
     name                 = "await-userdata"
     default_result       = "CONTINUE"
     heartbeat_timeout    = 300 # Wait up to 5 mins for UserData to finish
     lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
   }
-}
+
+# Instance Refresh Block
+  # This prevents 503 errors by ensuring new instances are ready before old ones die.
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      # Percentage of the ASG that must remain 'InService' and 'Healthy' during a refresh.
+      # Setting this to 50% means 3 out of your 6 desired instances stay alive.
+      min_healthy_percentage = 50
+      
+      # Time to wait after an instance is 'InService' before moving to the next one.
+      instance_warmup = 300 
+    }
+    # Refresh will trigger if the Launch Template or UserData changes.
+    triggers = ["tag"] 
+  }
+  
+} 
 
 # Auto Scaling Policy
 resource "aws_autoscaling_policy" "asg_policy" {
