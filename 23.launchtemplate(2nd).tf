41053c7264101a26f8284978990f9af9d10bb2a6
Nadir <the.fire.dragon.mac@gmail.com>
2026-01-13 18:10:17 -0500

    Add files via upload

--- 23.launchtemplate(2nd).tf
+++ 23.launchtemplate(2nd).tf
@@ -14,23 +14,26 @@ resource "aws_launch_template" "app_template_2nd" {
     security_groups             = [aws_security_group.Linux_Server_2nd.id]
   }
 
-# Added Metadata Options for IMDSv2 reliability
+  # Added Metadata Options for IMDSv2 reliability
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
   }
 
-   user_data = base64encode(templatefile("userdata.sh", {
-    db_endpoint = aws_rds_global_cluster.global_db.endpoint
+  user_data = base64encode(templatefile("userdata.sh", {
+    db_endpoint = aws_rds_cluster.secondary_cluster.reader_endpoint
     db_name     = aws_rds_global_cluster.global_db.database_name
     db_user     = aws_rds_cluster.primary_cluster.master_username
     db_password = aws_secretsmanager_secret_version.db_password_val.secret_string
     asg_name    = "vanish-app-asg-secondary"
-}
-)
-)
-  depends_on = [aws_nat_gateway.nat_2nd]
+    }
+    )
+  )
+
+  # Ensure the London replica is up before London EC2s start
+  depends_on = [aws_rds_cluster_instance.secondary_instances,
+  aws_nat_gateway.nat_2nd]
 
   lifecycle {
     create_before_destroy = true
